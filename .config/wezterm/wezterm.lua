fb7c11a751d401b8776b3b0ffb38abedf1652cf8
Daniel Correia <danielgalego.dev@gmail.com>
2024-03-25 03:00:05 -0300

    feat: add skhd yabai changes when moving between displays and wezterm workspaces

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -9,7 +9,7 @@ config.font_size = 14.5
 config.color_scheme = "GruvboxDarkHard"
 
 -- Tab bar
-config.hide_tab_bar_if_only_one_tab = true
+config.hide_tab_bar_if_only_one_tab = false
 config.enable_tab_bar = true
 config.tab_bar_at_bottom = true
 config.window_padding = {
@@ -22,7 +22,8 @@ config.use_fancy_tab_bar = false
 
 local SOLID_LEFT_ARROW = wezterm.nerdfonts.pl_right_hard_divider
 local SOLID_RIGHT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
-
+local SOLID_SQUARE = wezterm.nerdfonts.fa_square
+local HALF_CIRLCE_RIGHT = wezterm.nerdfonts.ple_right_half_circle_thick
 -- This function returns the suggested title for a tab.
 -- It prefers the title that was set via `tab:set_title()`
 -- or `wezterm cli set-tab-title`, but falls back to the
@@ -40,14 +41,14 @@ end
 
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
 	local edge_background = "#0b0022"
-	local background = "#1b1032"
+	local background = "#1E1E24"
 	local foreground = "#808080"
 
 	if tab.is_active then
-		background = "#2b2042"
+		background = "#323238"
 		foreground = "#c0c0c0"
 	elseif hover then
-		background = "#3b3052"
+		background = "#3c3a39"
 		foreground = "#909090"
 	end
 
@@ -59,16 +60,29 @@ wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_wid
 	-- and that we have room for the edges.
 	title = wezterm.truncate_right(title, max_width - 2)
 
+	if tab.tab_index == #tabs - 1 then
+		return {
+			{ Background = { Color = edge_background } },
+			{ Foreground = { Color = edge_foreground } },
+			{ Background = { Color = background } },
+			{ Foreground = { Color = foreground } },
+			{ Text = " " .. title .. " " },
+			{ Background = { Color = edge_background } },
+			{ Foreground = { Color = edge_foreground } },
+			{ Text = SOLID_RIGHT_ARROW },
+		}
+	end
+
 	return {
 		{ Background = { Color = edge_background } },
 		{ Foreground = { Color = edge_foreground } },
-		{ Text = SOLID_LEFT_ARROW },
+		-- { Text = " " .. title },
 		{ Background = { Color = background } },
 		{ Foreground = { Color = foreground } },
-		{ Text = title },
-		{ Background = { Color = edge_background } },
-		{ Foreground = { Color = edge_foreground } },
-		{ Text = SOLID_RIGHT_ARROW },
+		{ Text = " " .. title .. " " },
+		-- { Background = { Color = edge_background } },
+		-- { Foreground = { Color = edge_foreground } },
+		-- { Text = SOLID_RIGHT_ARROW },
 	}
 end)
 
@@ -79,45 +93,45 @@ wezterm.on("update-right-status", function(window, pane)
 	-- Figure out the cwd and host of the current pane.
 	-- This will pick up the hostname for the remote host if your
 	-- shell is using OSC 7 on the remote host.
-	local cwd_uri = pane:get_current_working_dir()
-	if cwd_uri then
-		local cwd = ""
-		local hostname = ""
-
-		if type(cwd_uri) == "userdata" then
-			-- Running on a newer version of wezterm and we have
-			-- a URL object here, making this simple!
-
-			cwd = cwd_uri.file_path
-			hostname = cwd_uri.host or wezterm.hostname()
-		else
-			-- an older version of wezterm, 20230712-072601-f4abf8fd or earlier,
-			-- which doesn't have the Url object
-			cwd_uri = cwd_uri:sub(8)
-			local slash = cwd_uri:find("/")
-			if slash then
-				hostname = cwd_uri:sub(1, slash - 1)
-				-- and extract the cwd from the uri, decoding %-encoding
-				cwd = cwd_uri:sub(slash):gsub("%%(%x%x)", function(hex)
-					return string.char(tonumber(hex, 16))
-				end)
-			end
-		end
-
-		-- Remove the domain name portion of the hostname
-		local dot = hostname:find("[.]")
-		if dot then
-			hostname = hostname:sub(1, dot - 1)
-		end
-		if hostname == "" then
-			hostname = wezterm.hostname()
-		end
-
-		-- table.insert(cells, cwd)
-		-- table.insert(cells, hostname)
-	end
-
+	-- local cwd_uri = pane:get_current_working_dir()
+	-- if cwd_uri then
+	-- 	local cwd = ""
+	-- 	local hostname = ""
+	--
+	-- 	if type(cwd_uri) == "userdata" then
+	-- 		-- Running on a newer version of wezterm and we have
+	-- 		-- a URL object here, making this simple!
+	--
+	-- 		cwd = cwd_uri.file_path
+	-- 		hostname = cwd_uri.host or wezterm.hostname()
+	-- 	else
+	-- 		-- an older version of wezterm, 20230712-072601-f4abf8fd or earlier,
+	-- 		-- which doesn't have the Url object
+	-- 		cwd_uri = cwd_uri:sub(8)
+	-- 		local slash = cwd_uri:find("/")
+	-- 		if slash then
+	-- 			hostname = cwd_uri:sub(1, slash - 1)
+	-- 			-- and extract the cwd from the uri, decoding %-encoding
+	-- 			cwd = cwd_uri:sub(slash):gsub("%%(%x%x)", function(hex)
+	-- 				return string.char(tonumber(hex, 16))
+	-- 			end)
+	-- 		end
+	-- 	end
+	--
+	-- 	-- Remove the domain name portion of the hostname
+	-- 	local dot = hostname:find("[.]")
+	-- 	if dot then
+	-- 		hostname = hostname:sub(1, dot - 1)
+	-- 	end
+	-- 	if hostname == "" then
+	-- 		hostname = wezterm.hostname()
+	-- 	end
+	--
+	-- 	-- table.insert(cells, cwd)
+	-- 	-- table.insert(cells, hostname)
+	-- end
 	-- I like my date/time in this style: "Wed Mar 3 08:14"
+
 	local date = wezterm.strftime("%a %b %-d %H:%M")
 	table.insert(cells, date)
 
@@ -127,18 +141,35 @@ wezterm.on("update-right-status", function(window, pane)
 	end
 
 	-- The powerline < symbol
-	local LEFT_ARROW = utf8.char(0xe0b3)
+	-- local LEFT_ARROW = utf8.char(0xe0b3)
 	-- The filled in variant of the < symbol
-	local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+	local SOLID_LEFT_ARROW_STATUS = utf8.char(0xe0b2)
 
 	-- Color palette for the backgrounds of each cell
 	local colors = {
-		"#3c1361",
-		"#52307c",
-		"#663a82",
-		"#7c5295",
-		"#b491c8",
+		"#323238",
+		"#1E1E24",
+		-- "#7c5295",
+		-- "#b491c8",
 	}
+
+	local ROBOT_ICON = wezterm.nerdfonts.oct_dependabot
+	local SATURN_ICON = wezterm.nerdfonts.fae_planet
+
+	local workspace_icon = ""
+	if window:active_workspace() == "default" then
+		workspace_icon = SATURN_ICON
+	else
+		workspace_icon = ROBOT_ICON
+	end
+
+	table.insert(cells, window:active_workspace() .. " " .. workspace_icon)
+
+	if window:active_workspace() == "default" then
+		table.insert(colors, "#0d0e0f")
+	else
+		table.insert(colors, "#D65d0e")
+	end
 
 	-- Foreground color for the text across the fade
 	local text_fg = "#c0c0c0"
@@ -149,25 +180,33 @@ wezterm.on("update-right-status", function(window, pane)
 	local num_cells = 0
 
 	-- Translate a cell into elements
-	function push(text, is_last)
+	function push(text, is_last, is_first)
 		local cell_no = num_cells + 1
+		if is_first then
+			table.insert(elements, { Foreground = { Color = colors[cell_no] } })
+			table.insert(elements, { Text = SOLID_LEFT_ARROW_STATUS })
+		end
+
 		table.insert(elements, { Foreground = { Color = text_fg } })
 		table.insert(elements, { Background = { Color = colors[cell_no] } })
 		table.insert(elements, { Text = " " .. text .. " " })
 		if not is_last then
 			table.insert(elements, { Foreground = { Color = colors[cell_no + 1] } })
-			table.insert(elements, { Text = SOLID_LEFT_ARROW })
+			table.insert(elements, { Text = SOLID_LEFT_ARROW_STATUS })
 		end
 		num_cells = num_cells + 1
 	end
 
+	local is_first = true
 	while #cells > 0 do
 		local cell = table.remove(cells, 1)
-		push(cell, #cells == 0)
+		push(cell, #cells == 0, is_first)
+		is_first = false
 	end
 
 	window:set_right_status(wezterm.format(elements))
 end)
+config.show_new_tab_button_in_tab_bar = false
 
 config.colors = {
 	tab_bar = {
@@ -175,11 +214,12 @@ config.colors = {
 	},
 	cursor_bg = "#FABD2f",
 	cursor_border = "#EEBD35",
+	background = "#0D0E0F",
 }
 
 config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.45
-config.macos_window_background_blur = 20
+config.window_background_opacity = 0.6
+config.macos_window_background_blur = 15
 
 -- Cursor
 config.default_cursor_style = "BlinkingBlock"
@@ -274,6 +314,37 @@ config.keys = {
 	split_nav("resize", "j"),
 	split_nav("resize", "k"),
 	split_nav("resize", "l"),
+	{
+		key = "w",
+		mods = "CTRL|CMD",
+		action = wezterm.action.PromptInputLine({
+			description = wezterm.format({
+				{ Attribute = { Intensity = "Bold" } },
+				{ Foreground = { AnsiColor = "Fuchsia" } },
+				{ Text = "Enter name for new workspace" },
+			}),
+			action = wezterm.action_callback(function(window, pane, line)
+				-- line will be `nil` if they hit escape without entering anything
+				-- An empty string if they just hit enter
+				-- Or the actual line of text they wrote
+				if line then
+					window:perform_action(
+						wezterm.action.SwitchToWorkspace({
+							name = line,
+						}),
+						pane
+					)
+				end
+			end),
+		}),
+	},
+	{
+		key = "y",
+		mods = "CTRL|CMD",
+		action = wezterm.action.SwitchToWorkspace({
+			name = "default",
+		}),
+	},
 }
 -- config.keys = {
 -- 	-- move between split panes
