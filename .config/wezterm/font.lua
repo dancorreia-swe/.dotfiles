a6cb4ed291905e3632dc896e1f3ac4681a6e8114
Daniel Correia <danielgalego.dev@gmail.com>
2024-04-05 22:32:51 -0300

    feat: adjust font on wezterm

--- .config/wezterm/font.lua
+++ .config/wezterm/font.lua
@@ -3,8 +3,10 @@ local wezterm = require("wezterm")
 local module = {}
 
 function module.apply_to_config(config)
-	config.font = wezterm.font("JetBrains Mono")
-	config.font_size = 15
+	config.font = wezterm.font_with_fallback({ "JetBrainsMono Nerd Font", "SF Mono" })
+	config.font_size = 14
+	config.cell_width = 0.9
+	config.freetype_load_flags = "NO_HINTING"
 end
 
 return module
