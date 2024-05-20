5fb9d672d3c3f38a0778693754afb3c71e25aded
Daniel Correia <danielgalego.dev@gmail.com>
2024-05-20 02:33:28 -0300

    feat(neorg): additional configurations and keybinds

--- .config/nvim/lua/custom/plugins/neorg.lua
+++ .config/nvim/lua/custom/plugins/neorg.lua
@@ -6,12 +6,36 @@ return {
   },
   {
     'nvim-neorg/neorg',
-    dependencies = { 'luarocks.nvim' },
+    dependencies = {
+      'luarocks.nvim',
+      'nvim-lua/plenary.nvim',
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-tree/nvim-web-devicons',
+    },
+    build = ':Neorg sync-parsers',
+    cmd = { 'Neorg' },
     lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
     version = '*', -- Pin Neorg to the latest stable release
     config = function()
       require('neorg').setup {
         load = {
+          ['core.keybinds'] = {
+            config = {
+              neorg_leader = ',',
+              hook = function(keybinds)
+                keybinds.remap_event('norg', 'i', '<S-CR>', 'core.itero.next-iteration')
+                keybinds.map('norg', 'n', keybinds.leader .. 'jt', '<cmd>Neorg journal today<CR>', { desc = '[neorg] journal today' })
+                keybinds.map('norg', 'n', keybinds.leader .. 'jp', '<cmd>Neorg journal template<CR>', { desc = '[neorg] journal template' })
+                keybinds.map('norg', 'n', keybinds.leader .. 'jT', '<cmd>Neorg journal tomorrow<CR>', { desc = '[neorg] journal tomorrow' })
+                keybinds.map('norg', 'n', keybinds.leader .. 'jy', '<cmd>Neorg journal yesterday<CR>', { desc = '[neorg] journal yesterday' })
+                keybinds.map_event_to_mode('norg', {
+                  n = {
+                    { keybinds.leader .. 'ge', 'core.esupports.hop.hop-link', 'external', opts = { desc = 'Open External Link' } },
+                  },
+                }, {})
+              end,
+            },
+          },
           ['core.defaults'] = {},
           ['core.completion'] = {
             config = {
@@ -28,6 +52,9 @@ return {
               default_workspace = 'notes',
             },
           },
+          ['core.highlights'] = {},
+          ['core.summary'] = {},
+          ['core.ui.calendar'] = {},
         },
       }
 
