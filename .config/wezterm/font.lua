272684c05dfd74502ef3caabb74c74ee0d41bd7c
Daniel Correia <danielgalego.dev@gmail.com>
2024-09-09 17:37:34 -0300

    feat: make wezterm font smaller by default

--- .config/wezterm/font.lua
+++ .config/wezterm/font.lua
@@ -4,9 +4,10 @@ local module = {}
 
 function module.apply_to_config(config)
 	config.font = wezterm.font_with_fallback({ "JetBrainsMono Nerd Font", "SF Mono" })
-	config.font_size = 15
+	config.font_size = 14
 	config.cell_width = 0.9
 	config.freetype_load_flags = "NO_HINTING"
+	config.front_end = "OpenGL"
 end
 
 return module
