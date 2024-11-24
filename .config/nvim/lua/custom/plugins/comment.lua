aaaa8ae6daeed5d68b9e194de4d404896f9bfeac
Daniel Correia <danielgalego.dev@gmail.com>
2024-11-24 14:13:27 -0300

    feat(nvim): add ts-comment and namespace php

--- .config/nvim/lua/custom/plugins/comment.lua
+++ .config/nvim/lua/custom/plugins/comment.lua
@@ -12,5 +12,11 @@ return {
   dependencies = {
     'nvim-treesitter/nvim-treesitter',
     'JoosepAlviste/nvim-ts-context-commentstring',
+    {
+      'folke/ts-comments.nvim',
+      opts = {},
+      event = 'VeryLazy',
+      enabled = vim.fn.has 'nvim-0.10.0' == 1,
+    },
   },
 }
