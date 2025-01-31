848c7b8d4c57476200eecaff2ba961f633cc0615
Daniel Correia <danielgalego.dev@gmail.com>
2025-01-30 21:23:48 -0300

    fix(nvim): hotkeys to swap between buffers
    bufferline update/fix

--- .config/nvim/lua/custom/plugins/ui/bufferline.lua
+++ .config/nvim/lua/custom/plugins/ui/bufferline.lua
@@ -16,7 +16,7 @@ return {
     }
 
     vim.keymap.set('n', '<leader><tab>', '<Cmd>BufferLinePick<CR>', { desc = 'Pick Buffer [T]ab', silent = true })
-    vim.keymap.set('n', '<C-;>', '<Cmd>BufferLineCycleNext<CR>', { desc = 'Cycle Buffer Tab Next' })
-    vim.keymap.set('n', "<C-'>", '<Cmd>BufferLineCyclePrev<CR>', { desc = 'Cycle Buffer Tab Prev' })
+    vim.keymap.set('n', "<C-'>", '<Cmd>BufferLineCycleNext<CR>', { desc = 'Cycle Buffer Tab Next' })
+    vim.keymap.set('n', '<C-;>', '<Cmd>BufferLineCyclePrev<CR>', { desc = 'Cycle Buffer Tab Prev' })
   end,
 }
