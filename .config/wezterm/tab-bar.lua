e91ba451cafcc04070d15b55a33998c9abc754dc
Daniel Correia <100008358+dancorreia-swe@users.noreply.github.com>
2025-02-04 22:24:19 -0300

    feat 💄: change tab bar style to fancy

--- .config/wezterm/tab-bar.lua
+++ .config/wezterm/tab-bar.lua
@@ -3,9 +3,6 @@ local wezterm = require("wezterm")
 local module = {}
 
 function module.apply_to_config(config)
-	-- Tab bar
-	config.hide_tab_bar_if_only_one_tab = false
-	config.enable_tab_bar = true
 	config.tab_bar_at_bottom = true
 	config.window_padding = {
 		left = 8,
@@ -13,15 +10,25 @@ function module.apply_to_config(config)
 		top = 8,
 		bottom = 8,
 	}
-	config.use_fancy_tab_bar = false
+	config.use_fancy_tab_bar = true
+	config.hide_tab_bar_if_only_one_tab = false
+	config.show_new_tab_button_in_tab_bar = false
+	config.tab_and_split_indices_are_zero_based = false
 
-	local SOLID_RIGHT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
+	config.window_frame = {
+		inactive_titlebar_bg = "none",
+		active_titlebar_bg = "none",
+	}
 
-	-- This function returns the suggested title for a tab.
-	-- It prefers the title that was set via `tab:set_title()`
-	-- or `wezterm cli set-tab-title`, but falls back to the
-	-- title of the active pane in that tab.
-	function tab_title(tab_info)
+	local LEFT_END = utf8.char(0xE0B6)
+	local RIGHT_END = utf8.char(0xE0B4)
+
+	local active_tab_bg_color = "#FFA066"
+	local inactive_tab_text_color = "#526F76"
+	local active_tab_fg_color = "#22222B" --'#051321' --'#15151A' --'#001C23'
+	local inactive_tab_bg_color = "#22222B"
+
+	local function tab_title(tab_info)
 		local title = tab_info.tab_title
 		-- if the tab title is explicitly set, take that
 		if title and #title > 0 then
@@ -33,53 +40,49 @@ function module.apply_to_config(config)
 	end
 
 	wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-		local edge_background = "rgba(0, 0, 0, 0.2)"
-		local background = "#1E1E24"
-		local foreground = "#808080"
-
-		if tab.is_active then
-			background = "#323238"
-			foreground = "#c0c0c0"
-		elseif hover then
-			background = "#3c3a39"
-			foreground = "#909090"
-		end
-
-		local edge_foreground = background
-
 		local title = tab_title(tab)
 
-		-- ensure that the titles fit in the available space,
-		-- and that we have room for the edges.
 		title = wezterm.truncate_right(title, max_width - 2)
 
-		if tab.tab_index == #tabs - 1 then
-			return {
-				{ Background = { Color = edge_background } },
-				{ Foreground = { Color = edge_foreground } },
-				{ Background = { Color = background } },
-				{ Foreground = { Color = foreground } },
-				{ Text = " " .. title .. " " },
-				{ Background = { Color = edge_background } },
-				{ Foreground = { Color = edge_foreground } },
-				{ Text = SOLID_RIGHT_ARROW },
-			}
-		end
+		local main_bg_color = "#22222B"
+		local background = "#22222B"
+		local tab_icon_inactive = "#495267"
+		local tab_icon_inactive_icon = wezterm.nerdfonts.md_ghost_off_outline
+		local tab_icon_active_icon = wezterm.nerdfonts.md_ghost
 
+		local icon_text = ""
+		local tab_icon_color = ""
+		local tab_text_color = ""
+		local tab_background_color = ""
+
+		if tab.is_active then
+			tab_icon_color = active_tab_fg_color
+			tab_text_color = active_tab_fg_color
+			tab_background_color = active_tab_bg_color
+			icon_text = tab_icon_active_icon
+		else
+			tab_icon_color = tab_icon_inactive
+			tab_text_color = inactive_tab_text_color
+			icon_text = tab_icon_inactive_icon
+			tab_background_color = inactive_tab_bg_color
+		end
+		-- ensure that the titles fit in the available space,
+		-- and that we have room for the edges.
 		return {
-			{ Background = { Color = edge_background } },
-			{ Foreground = { Color = edge_foreground } },
-			-- { Text = " " .. title },
+			{ Background = { Color = main_bg_color } },
+			{ Foreground = { Color = tab_background_color } },
+			{ Text = LEFT_END },
+			{ Background = { Color = tab_background_color } },
+			{ Foreground = { Color = tab_icon_color } },
+			{ Text = " " .. icon_text .. "  " },
+			{ Background = { Color = tab_background_color } },
+			{ Foreground = { Color = tab_text_color } },
+			{ Text = title .. "  " },
 			{ Background = { Color = background } },
-			{ Foreground = { Color = foreground } },
-			{ Text = " " .. title .. " " },
-			-- { Background = { Color = edge_background } },
-			-- { Foreground = { Color = edge_foreground } },
-			-- { Text = SOLID_RIGHT_ARROW },
+			{ Foreground = { Color = tab_background_color } },
+			{ Text = RIGHT_END },
 		}
 	end)
-
-	config.show_new_tab_button_in_tab_bar = false
 end
 
 return module
