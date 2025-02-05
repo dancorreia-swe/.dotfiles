8affa2bb031e66363170ecf4b53ac39359716995
Daniel Correia <100008358+dancorreia-swe@users.noreply.github.com>
2025-02-04 22:36:53 -0300

    feat: remove bg from tab-line and move window property to the right file

--- .config/wezterm/tab-bar.lua
+++ .config/wezterm/tab-bar.lua
@@ -4,12 +4,6 @@ local module = {}
 
 function module.apply_to_config(config)
 	config.tab_bar_at_bottom = true
-	config.window_padding = {
-		left = 8,
-		right = 8,
-		top = 8,
-		bottom = 8,
-	}
 	config.use_fancy_tab_bar = true
 	config.hide_tab_bar_if_only_one_tab = false
 	config.show_new_tab_button_in_tab_bar = false
@@ -23,9 +17,9 @@ function module.apply_to_config(config)
 	local LEFT_END = utf8.char(0xE0B6)
 	local RIGHT_END = utf8.char(0xE0B4)
 
-	local active_tab_bg_color = "#FFA066"
-	local inactive_tab_text_color = "#526F76"
-	local active_tab_fg_color = "#22222B" --'#051321' --'#15151A' --'#001C23'
+	local active_tab_bg_color = "#bac2de"
+	local inactive_tab_text_color = "#585b70"
+	local active_tab_fg_color = "#22222B"
 	local inactive_tab_bg_color = "#22222B"
 
 	local function tab_title(tab_info)
@@ -44,9 +38,9 @@ function module.apply_to_config(config)
 
 		title = wezterm.truncate_right(title, max_width - 2)
 
-		local main_bg_color = "#22222B"
-		local background = "#22222B"
-		local tab_icon_inactive = "#495267"
+		local main_bg_color = "none"
+		local background = "none"
+		local tab_icon_inactive = "#45475a"
 		local tab_icon_inactive_icon = wezterm.nerdfonts.md_ghost_off_outline
 		local tab_icon_active_icon = wezterm.nerdfonts.md_ghost
 
@@ -60,28 +54,36 @@ function module.apply_to_config(config)
 			tab_text_color = active_tab_fg_color
 			tab_background_color = active_tab_bg_color
 			icon_text = tab_icon_active_icon
+
+			return {
+				{ Background = { Color = main_bg_color } },
+				{ Foreground = { Color = tab_background_color } },
+				{ Text = LEFT_END },
+				{ Background = { Color = tab_background_color } },
+				{ Foreground = { Color = tab_icon_color } },
+				{ Text = " " .. icon_text .. "   " },
+				{ Background = { Color = tab_background_color } },
+				{ Foreground = { Color = tab_text_color } },
+				{ Text = title .. "  " },
+				{ Background = { Color = background } },
+				{ Foreground = { Color = tab_background_color } },
+				{ Text = RIGHT_END },
+			}
 		else
 			tab_icon_color = tab_icon_inactive
 			tab_text_color = inactive_tab_text_color
 			icon_text = tab_icon_inactive_icon
 			tab_background_color = inactive_tab_bg_color
+
+			return {
+				{ Background = { Color = main_bg_color } },
+				{ Foreground = { Color = tab_icon_color } },
+				{ Text = " " .. icon_text .. "   " },
+				{ Foreground = { Color = tab_text_color } },
+				{ Text = title .. "  " },
+				{ Foreground = { Color = tab_background_color } },
+			}
 		end
-		-- ensure that the titles fit in the available space,
-		-- and that we have room for the edges.
-		return {
-			{ Background = { Color = main_bg_color } },
-			{ Foreground = { Color = tab_background_color } },
-			{ Text = LEFT_END },
-			{ Background = { Color = tab_background_color } },
-			{ Foreground = { Color = tab_icon_color } },
-			{ Text = " " .. icon_text .. "  " },
-			{ Background = { Color = tab_background_color } },
-			{ Foreground = { Color = tab_text_color } },
-			{ Text = title .. "  " },
-			{ Background = { Color = background } },
-			{ Foreground = { Color = tab_background_color } },
-			{ Text = RIGHT_END },
-		}
 	end)
 end
 
--- .config/wezterm/window.lua
+++ .config/wezterm/window.lua
@@ -4,6 +4,12 @@ function module.apply_to_config(config)
 	config.window_decorations = "RESIZE"
 	config.window_background_opacity = 0.92
 	config.macos_window_background_blur = 15
+	config.window_padding = {
+		left = 8,
+		right = 8,
+		top = 16,
+		bottom = 8,
+	}
 end
 
 return module
