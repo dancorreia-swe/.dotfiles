fa71e9ce142a4ef3792c3083c8fbd37c0f42699d
Daniel Correia <100008358+dancorreia-swe@users.noreply.github.com>
2025-03-04 01:52:38 -0300

    chore(nvim): comment md lint

--- .config/nvim/lua/kickstart/plugins/lint.lua
+++ .config/nvim/lua/kickstart/plugins/lint.lua
@@ -1,12 +1,11 @@
 return {
-
   { -- Linting
     'mfussenegger/nvim-lint',
     event = { 'BufReadPre', 'BufNewFile' },
     config = function()
       local lint = require 'lint'
       lint.linters_by_ft = {
-        markdown = { 'markdownlint' },
+        -- markdown = { 'markdownlint-cli2' },
         elixir = { 'credo' },
         typescript = { 'eslint_d' },
         typescriptreact = { 'eslint_d' },
