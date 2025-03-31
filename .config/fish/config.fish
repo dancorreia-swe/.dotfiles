afe27bc2756c18c4b45dbbaf65a0346548430a46
Daniel Correia <100008358+dancorreia-swe@users.noreply.github.com>
2025-03-31 10:26:50 -0300

    chore(fish): add formating to config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,11 @@
 if status is-interactive
-fish_add_path /Users/danielmac/.local/bin
-fish_add_path /opt/homebrew/bin
-fish_add_path "$HOME/.dotfiles/bin"
-fish_add_path "$HOME/.local/bin"
+  fish_add_path /Users/danielmac/.local/bin
+  fish_add_path /opt/homebrew/bin
+  fish_add_path "$HOME/.dotfiles/bin"
+  fish_add_path "$HOME/.local/bin"
 
-oh-my-posh init fish --config $HOME/.config/ohmyposh/zen.toml | source
+  oh-my-posh init fish --config $HOME/.config/ohmyposh/zen.toml | source
 
-zoxide init fish | source
+  zoxide init fish | source
 end
 
