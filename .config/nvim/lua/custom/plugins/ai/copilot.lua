b878b206608fb136599f4fb5165e0c733d0eabc0
Daniel Correia <100008358+dancorreia-swe@users.noreply.github.com>
2025-06-27 10:39:49 -0300

    feat: transition to blink

--- .config/nvim/lua/custom/plugins/ai/copilot.lua
+++ .config/nvim/lua/custom/plugins/ai/copilot.lua
@@ -1,14 +1,33 @@
 return {
-  { -- Copilot
+  {
     'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    event = 'InsertEnter',
     opts = {
       suggestion = { enabled = false },
       panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
+      },
     },
   },
-
   {
-    'zbirenbaum/copilot-cmp',
-    opts = {},
+    'saghen/blink.cmp',
+    optional = true,
+    dependencies = { 'fang2hou/blink-copilot' },
+    opts = {
+      sources = {
+        default = { 'copilot' },
+        providers = {
+          copilot = {
+            name = 'copilot',
+            module = 'blink-copilot',
+            score_offset = 100,
+            async = true,
+          },
+        },
+      },
+    },
   },
 }
