8d6c6500dd91108ca9ef0776443354bf6976e9af
Daniel Correia <100008358+dancorreia-swe@users.noreply.github.com>
2025-07-04 14:21:28 -0300

    lint: indent few keys

--- .config/sketchybar/items/calendar.lua
+++ .config/sketchybar/items/calendar.lua
@@ -5,32 +5,32 @@ local colors = require("colors")
 sbar.add("item", { position = "right", width = settings.group_paddings })
 
 local cal = sbar.add("item", {
-  icon = {
-    color = colors.white,
-    padding_left = 4,
-    padding_right = 8,
-    font = {
-      style = settings.font.style_map["Black"],
-      size = 12.0,
-    },
-  },
-  label = {
-    color = colors.white,
-    padding_right = 8,
-    width = 49,
-    align = "right",
-    font = { family = settings.font.numbers },
-  },
-  position = "right",
-  update_freq = 30,
-  padding_left = 1,
-  padding_right = 1,
-  background = {
-    color = colors.transparent,
-    border_color = colors.black,
-    border_width = 0
-  },
-  click_script = "open -a 'Calendar'"
+	icon = {
+		color = colors.white,
+		padding_left = 4,
+		padding_right = 8,
+		font = {
+			style = settings.font.style_map["Black"],
+			size = 12.0,
+		},
+	},
+	label = {
+		color = colors.white,
+		padding_right = 8,
+		width = 49,
+		align = "right",
+		font = { family = settings.font.numbers },
+	},
+	position = "right",
+	update_freq = 30,
+	padding_left = 1,
+	padding_right = 1,
+	background = {
+		color = colors.transparent,
+		border_color = colors.black,
+		border_width = 0,
+	},
+	click_script = "open -a 'Calendar'",
 })
 
 -- Double border for calendar using a single item bracket
@@ -46,5 +46,5 @@ local cal = sbar.add("item", {
 sbar.add("item", { position = "right", width = settings.group_paddings })
 
 cal:subscribe({ "forced", "routine", "system_woke" }, function(env)
-  cal:set({ icon = os.date("%a. %d %b"), label = os.date("%H:%M") })
+	cal:set({ icon = os.date("%a. %d %b"), label = os.date("%H:%M") })
 end)
