8a43fedb73040aa9c0ca880a65a347dc21089fdc
Daniel Correia <100008358+dancorreia-swe@users.noreply.github.com>
2025-07-05 22:26:19 -0300

    feat: add claude alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -27,3 +27,4 @@ end
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+alias claude="/Users/danielmac/.claude/local/claude"
