24bba8f94a1c034c306edf55a4c92c0f94d273d3
Daniel Correia <100008358+dancorreia-swe@users.noreply.github.com>
2025-08-15 16:33:15 -0300

    refactor: pass down plugins to correct domain directories

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -61,12 +61,12 @@ require('lazy').setup({
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  { import = 'custom.plugins' },
   { import = 'custom.plugins.ai' },
-  { import = 'custom.plugins.editor' },
-  { import = 'custom.plugins.ui' },
+  { import = 'custom.plugins.lsp' },
   { import = 'custom.plugins.coding' },
+  { import = 'custom.plugins.editor' },
   { import = 'custom.plugins.lang' },
+  { import = 'custom.plugins.ui' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
--- .config/nvim/lua/custom/plugins/editor/obsidian.lua
+++ .config/nvim/lua/custom/plugins/editor/obsidian.lua
@@ -25,131 +25,112 @@ return {
     { '<leader>ot', '<CMD>Obsidian tags<CR>', desc = 'Open Obsidian tag picker' },
     { '<leader>ob', '<CMD>Obsidian backlinks<CR>', desc = 'Open Obsidian backlinks picker' },
     { '<leader>on', '<CMD>Obsidian new<CR>', desc = 'Obsidian new note' },
+    { '<leader>on', '<CMD>Obsidian link_new<CR>', desc = 'Obsidian link note', mode = { 'n', 'v' } },
   },
-  config = function()
-    require('obsidian').setup {
-      workspaces = {
-        {
-          name = 'personal',
-          path = '~/vaults/personal',
-          overrides = {
-            notes_subdir = 'slip-box',
-          },
-        },
-        {
-          name = 'work',
-          path = '~/vaults/work',
+  opts = {
+    workspaces = {
+      {
+        name = 'personal',
+        path = '~/vaults/personal',
+        overrides = {
+          notes_subdir = 'slip-box',
         },
       },
-
-      new_notes_location = 'notes_subdir',
-      daily_notes = {
-        -- Optional, if you keep daily notes in a separate directory.
-        folder = 'daily notes',
-        -- Optional, if you want to change the date format for the ID of daily notes.
-        date_format = '%Y-%m-%d',
-        -- Optional, if you want to change the date format of the default alias of daily notes.
-        alias_format = '%B %-d, %Y',
-        -- Optional, default tags to add to each new daily note created.
-        default_tags = { 'daily-notes' },
-        -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
-        template = nil,
+      {
+        name = 'work',
+        path = '~/vaults/work',
       },
+    },
 
-      log_level = vim.log.levels.INFO, -- see below for full list of options 👇
-      completion = {
-        blink = true,
-        min_chars = 2,
-        create_new = true,
-      },
-      -- Optional, customize how note IDs are generated given an optional title.
-      ---@param title string|?
-      ---@return string
-      note_id_func = function(title)
-        -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
-        -- In this case a note with the title 'My new note' will be given an ID that looks
-        -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'.
-        -- You may have as many periods in the note ID as you'd like—the ".md" will be added automatically
-        local suffix = ''
-        if title ~= nil then
-          -- If title is given, transform it into valid file name.
-          suffix = title:gsub(' ', '-'):gsub('[^A-Za-z0-9-]', ''):lower()
-        else
-          -- If title is nil, just add 4 random uppercase letters to the suffix.
-          for _ = 1, 4 do
-            suffix = suffix .. string.char(math.random(65, 90))
-          end
+    legacy_commands = false,
+
+    new_notes_location = 'notes_subdir',
+    daily_notes = {
+      -- Optional, if you keep daily notes in a separate directory.
+      folder = 'daily notes',
+      -- Optional, if you want to change the date format for the ID of daily notes.
+      date_format = '%Y-%m-%d',
+      -- Optional, if you want to change the date format of the default alias of daily notes.
+      alias_format = '%B %-d, %Y',
+      -- Optional, default tags to add to each new daily note created.
+      default_tags = { 'daily-notes' },
+      -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+      template = nil,
+    },
+
+    log_level = vim.log.levels.INFO, -- see below for full list of options 👇
+    completion = {
+      blink = true,
+      min_chars = 2,
+      create_new = true,
+    },
+
+    -- Optional, customize how note IDs are generated given an optional title.
+    ---@param title string|?
+    ---@return string
+    note_id_func = function(title)
+      -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+      -- In this case a note with the title 'My new note' will be given an ID that looks
+      -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'.
+      -- You may have as many periods in the note ID as you'd like—the ".md" will be added automatically
+      local suffix = ''
+      if title ~= nil then
+        -- If title is given, transform it into valid file name.
+        suffix = title:gsub(' ', '-'):gsub('[^A-Za-z0-9-]', ''):lower()
+      else
+        -- If title is nil, just add 4 random uppercase letters to the suffix.
+        for _ = 1, 4 do
+          suffix = suffix .. string.char(math.random(65, 90))
         end
+      end
 
-        return tostring(os.date '%Y%m%d') .. '-' .. suffix
+      return tostring(os.date '%Y%m%d') .. '-' .. suffix
+    end,
+
+    sort_by = 'modified',
+    sort_reversed = true,
+
+    picker = {
+      name = 'snacks.pick',
+      -- Optional, configure key mappings for the picker. These are the defaults.
+      -- Not all pickers support all mappings.
+      note_mappings = {
+        -- Create a new note from your query.
+        new = '<C-x>',
+        -- Insert a link to the selected note.
+        insert_link = '<C-l>',
+      },
+      tag_mappings = {
+        -- Add tag(s) to current note.
+        tag_note = '<C-x>',
+        -- Insert a tag at the current location.
+        insert_tag = '<C-l>',
+      },
+    },
+
+    ---@class obsidian.config.AttachmentsOpts
+    ---
+    ---Default folder to save images to, relative to the vault root.
+    ---@field img_folder? string
+    ---
+    ---Default name for pasted images
+    ---@field img_name_func? fun(): string
+    ---
+    ---Default text to insert for pasted images
+    ---@field img_text_func? fun(client: obsidian.Client, path: obsidian.Path): string
+    ---
+    ---Whether to confirm the paste or not. Defaults to true.
+    ---@field confirm_img_paste? boolean
+    attachments = {
+      img_folder = 'assets/imgs',
+      img_text_func = function(client, path)
+        local encoded_path = require('obsidian.util').urlencode(path:vault_relative_path() or tostring(path))
+        return string.format('![%s](%s)', path.name, encoded_path)
       end,
-
-      mappings = {
-        -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
-        ['gf'] = {
-          action = function()
-            return require('obsidian').util.gf_passthrough()
-          end,
-          opts = { noremap = false, expr = true, buffer = true },
-        },
-        -- Toggle check-boxes.
-        ['<leader>ch'] = {
-          action = function()
-            return require('obsidian').util.toggle_checkbox()
-          end,
-          opts = { buffer = true },
-        },
-        -- Smart action depending on context, either follow link or toggle checkbox.
-        ['<cr>'] = {
-          action = function()
-            return require('obsidian').util.smart_action()
-          end,
-          opts = { buffer = true, expr = true },
-        },
-      },
-
-      picker = {
-        name = 'snacks.pick',
-        -- Optional, configure key mappings for the picker. These are the defaults.
-        -- Not all pickers support all mappings.
-        note_mappings = {
-          -- Create a new note from your query.
-          new = '<C-x>',
-          -- Insert a link to the selected note.
-          insert_link = '<C-l>',
-        },
-        tag_mappings = {
-          -- Add tag(s) to current note.
-          tag_note = '<C-x>',
-          -- Insert a tag at the current location.
-          insert_tag = '<C-l>',
-        },
-      },
-
-      ---@class obsidian.config.AttachmentsOpts
-      ---
-      ---Default folder to save images to, relative to the vault root.
-      ---@field img_folder? string
-      ---
-      ---Default name for pasted images
-      ---@field img_name_func? fun(): string
-      ---
-      ---Default text to insert for pasted images
-      ---@field img_text_func? fun(client: obsidian.Client, path: obsidian.Path): string
-      ---
-      ---Whether to confirm the paste or not. Defaults to true.
-      ---@field confirm_img_paste? boolean
-      attachments = {
-        img_folder = 'assets/imgs',
-        img_text_func = function(client, path)
-          local encoded_path = require('obsidian.util').urlencode(path:vault_relative_path() or tostring(path))
-          return string.format('![%s](%s)', path.name, encoded_path)
-        end,
-        img_name_func = function()
-          return string.format('Pasted image %s', os.date '%Y%m%d%H%M%S')
-        end,
-        confirm_img_paste = true,
-      },
-    }
-  end,
+      img_name_func = function()
+        return string.format('Pasted image %s', os.date '%Y%m%d%H%M%S')
+      end,
+      confirm_img_paste = true,
+    },
+  },
 }
