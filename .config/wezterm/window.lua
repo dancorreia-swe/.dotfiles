cc91183d95fdd7612d3e7dd6b832c5d9079c3409
Daniel Correia <100008358+dancorreia-swe@users.noreply.github.com>
2025-09-21 16:42:46 -0300

    feat(wezterm): add webGpu as main rendering option
    damn, it's so much brighter and clear that's almost a shame I only
    figured this now.

--- .config/wezterm/window.lua
+++ .config/wezterm/window.lua
@@ -1,6 +1,7 @@
 local module = {}
 
 function module.apply_to_config(config)
+	config.front_end = "WebGpu"
 	config.window_decorations = "RESIZE"
 	config.window_background_opacity = 0.92
 	config.macos_window_background_blur = 15
