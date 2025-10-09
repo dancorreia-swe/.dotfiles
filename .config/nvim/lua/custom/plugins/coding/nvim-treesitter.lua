dfc6f5e355fe00b3efd96cacf845209c37141b22
Daniel Correia <100008358+dancorreia-swe@users.noreply.github.com>
2025-10-08 23:47:58 -0300

    feat(nvim): add treesitter context toggle
    I don't think it's going to be of much use, but it was easy to add.

--- .config/nvim/lua/custom/plugins/coding/nvim-treesitter.lua
+++ .config/nvim/lua/custom/plugins/coding/nvim-treesitter.lua
@@ -150,4 +150,30 @@ return {
     event = { 'BufReadPost', 'BufWritePost', 'BufNewFile' },
     opts = {},
   },
+
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    event = { 'BufReadPost', 'BufWritePost', 'BufNewFile' },
+    opts = function()
+      local tsc = require 'treesitter-context'
+
+      Snacks.toggle({
+        name = 'Treesitter Context',
+        get = tsc.enabled,
+        set = function(state)
+          if state then
+            tsc.enable()
+          else
+            tsc.disable()
+          end
+        end,
+      }):map '<leader>ut'
+
+      return {
+        enable = false,
+        mode = 'cursor',
+        max_lines = 3,
+      }
+    end,
+  },
 }
