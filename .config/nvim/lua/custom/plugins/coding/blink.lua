ce8f726b88a84bd67908e4b35c0becaa28ec5133
Daniel Correia <100008358+dancorreia-swe@users.noreply.github.com>
2025-10-15 10:02:26 -0300

    fix: change icon fonts and add border to hover docs

--- .config/nvim/lua/custom/plugins/coding/blink.lua
+++ .config/nvim/lua/custom/plugins/coding/blink.lua
@@ -38,7 +38,7 @@ return {
       use_nvim_cmp_as_default = false,
       -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
       -- adjusts spacing to ensure icons are aligned
-      nerd_font_variant = 'normal',
+      nerd_font_variant = 'mono',
     },
 
     completion = {
