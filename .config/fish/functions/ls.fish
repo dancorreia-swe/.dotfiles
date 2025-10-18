cec11980669f640167efbeeebefa6b5b7a16fda0
Daniel Correia <100008358+dancorreia-swe@users.noreply.github.com>
2025-10-18 19:18:20 -0300

    feat(fish): add ls helpers

--- .config/fish/functions/ls.fish
+++ .config/fish/functions/ls.fish
@@ -1,3 +1,28 @@
 function ls
-    eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions -G $argv
+    set -l opts --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions -G
+    eza $opts $argv
+end
+
+function la
+    set -l opts --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions -G
+    eza $opts -a $argv
+end
+
+function lf
+    eza --color=always --icons=always -G $argv
+end
+
+function lt
+    set -l opts --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions -G
+    eza $opts --sort=modified $argv
+end
+
+function ld
+    set -l opts --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions -G
+    eza $opts --only-dirs $argv
+end
+
+function lh
+    set -l opts --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions -G
+    eza $opts -a $argv | grep '^\.'
 end
