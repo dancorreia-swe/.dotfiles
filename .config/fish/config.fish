ea543fe8d184532990cefd80120c442f884f7eab
Daniel Correia <100008358+dancorreia-swe@users.noreply.github.com>
2025-11-21 20:19:38 -0300

    chore(fish): remove claude from shell alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -29,4 +29,3 @@ end
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
-alias claude="/Users/danielmac/.claude/local/claude"
