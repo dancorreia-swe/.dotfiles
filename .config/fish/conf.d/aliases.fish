13b2dfbe472a2373bd51b935820e9a3df261bc95
Daniel Correia <100008358+dancorreia-swe@users.noreply.github.com>
2025-11-21 20:20:12 -0300

    fix(fish): remove vim as abbr to nvim and simplify commands

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -1,8 +1,7 @@
 abbr -a C "z ~/Code"
-abbr -a lg "lazygit"
-abbr -a lzd "lazydocker"
-abbr -a ff "fastfetch"
-abbr -a nv "nvim"
-abbr -a vim "nvim"
-abbr -a cd "z"
+abbr -a lg lazygit
+abbr -a lzd lazydocker
+abbr -a ff fastfetch
+abbr -a nv nvim
+abbr -a cd z
 abbr -a sail 'sh $([ -f sail ] && echo sail || echo vendor/bin/sail)'
