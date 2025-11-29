de884ff421199106e0dc471688827f4bf89a761d
Daniel Correia <100008358+dancorreia-swe@users.noreply.github.com>
2025-11-29 12:22:49 -0300

    feat: add new util core functions
    shoutout to lazyvim and folke.

--- .config/nvim/lua/util/init.lua
+++ .config/nvim/lua/util/init.lua
@@ -136,4 +136,24 @@ function M.cwd()
   return M.realpath(vim.uv.cwd()) or ''
 end
 
+---@param name string
+function M.get_plugin(name)
+  return require('lazy.core.config').spec.plugins[name]
+end
+
+---@param plugin string
+function M.has(plugin)
+  return M.get_plugin(plugin) ~= nil
+end
+
+function M.opts(name)
+  local plugin = M.get_plugin(name)
+  if not plugin then
+    return {}
+  end
+
+  local Plugin = require 'lazy.core.plugin'
+  return Plugin.values(plugin, 'opts', false)
+end
+
 return M
