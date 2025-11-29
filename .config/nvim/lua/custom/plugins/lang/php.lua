b0693dd81f304bb40584789159cfe628330e2ebc
Daniel Correia <100008358+dancorreia-swe@users.noreply.github.com>
2025-11-29 12:23:35 -0300

    feat(nvim): add php lsp and tooling

---
+++ .config/nvim/lua/custom/plugins/lang/php.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    'mfussenegger/nvim-dap',
+    optional = true,
+    opts = function()
+      local dap = require 'dap'
+      dap.adapters.php = {
+        type = 'executable',
+        command = 'php-debug-adapter',
+        args = {},
+      }
+    end,
+  },
+  {
+    'nvim-neotest/neotest',
+    optional = true,
+    dependencies = {
+      'V13Axel/neotest-pest',
+      'olimorris/neotest-phpunit',
+    },
+    opts = {
+      adapters = {
+        'neotest-pest',
+        ['neotest-phpunit'] = {
+          root_ignore_files = { 'tests/Pest.php' },
+        },
+      },
+    },
+  },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    opts = { ensure_installed = { 'php' } },
+  },
+}
