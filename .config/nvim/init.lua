7327babcb3495054f51349c94efc2dd0f57c2261
Daniel Correia <100008358+dancorreia-swe@users.noreply.github.com>
2025-12-01 12:51:21 -0300

    refactor(nvim): rename custom plugin folder to plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -60,14 +60,14 @@ require('lazy').setup({
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  { import = 'custom.plugins.ai' },
-  { import = 'custom.plugins.lsp' },
-  { import = 'custom.plugins.coding' },
-  { import = 'custom.plugins.editor' },
-  { import = 'custom.plugins.dap' },
-  { import = 'custom.plugins.lang' },
-  { import = 'custom.plugins.ui' },
-  { import = 'custom.plugins.test' },
+  { import = 'plugins.ai' },
+  { import = 'plugins.lsp' },
+  { import = 'plugins.coding' },
+  { import = 'plugins.editor' },
+  { import = 'plugins.dap' },
+  { import = 'plugins.lang' },
+  { import = 'plugins.ui' },
+  { import = 'plugins.test' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
