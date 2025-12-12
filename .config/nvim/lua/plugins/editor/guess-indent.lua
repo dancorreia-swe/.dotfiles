24e70bbbeb5187e565cced0a057f58eb47fd8a57
Daniel Correia <100008358+dancorreia-swe@users.noreply.github.com>
2025-12-12 08:39:41 -0300

    refactor(nvim): combine single line plugins into a single file

--- .config/nvim/lua/plugins/editor/guess-indent.lua
+++
@@ -1,6 +0,0 @@
-return {
-  'nmac427/guess-indent.nvim',
-  event = { 'BufReadPost', 'BufNewFile', 'BufReadPre' },
-  cmd = 'GuessIndent',
-  opts = {},
-}
---
+++ .config/nvim/lua/plugins/editor/mini-plugins.lua
@@ -0,0 +1,31 @@
+-- Consolidated small plugin specs
+return {
+  -- Auto-close inactive buffers
+  {
+    'chrisgrieser/nvim-early-retirement',
+    config = true,
+    event = 'VeryLazy',
+  },
+
+  -- Session management
+  {
+    'folke/persistence.nvim',
+    event = 'BufReadPre',
+    opts = {},
+  },
+
+  -- Color highlighter
+  {
+    'catgoose/nvim-colorizer.lua',
+    event = 'BufReadPre',
+    opts = {},
+  },
+
+  -- Auto-detect indentation
+  {
+    'nmac427/guess-indent.nvim',
+    event = { 'BufReadPost', 'BufNewFile', 'BufReadPre' },
+    cmd = 'GuessIndent',
+    opts = {},
+  },
+}
