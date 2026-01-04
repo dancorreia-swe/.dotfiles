88f1cf4ec96bfa38e82a8003b933b78256fe25ee
Daniel Correia <100008358+dancorreia-swe@users.noreply.github.com>
2026-01-04 13:08:22 -0300

    add mole completions to fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -36,3 +36,7 @@ end
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+
+
+# Mole shell completion
+set -l output (mole completion fish 2>/dev/null); and echo "$output" | source
