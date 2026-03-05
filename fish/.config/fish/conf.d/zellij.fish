2725c7dda3a7b3773e7cdc663620dfe9a48d7b56
Daniel Correia <100008358+dancorreia-swe@users.noreply.github.com>
2026-03-05 12:27:16 -0300

    perf(fish): remove zellij tab rename hook, use built-in process naming
    Session name in zjstatus bar provides project context.
    Tabs now show the running command (nvim, fish, cargo, etc).
    Removes subprocess spawn on every cd.

--- fish/.config/fish/conf.d/zellij.fish
+++ fish/.config/fish/conf.d/zellij.fish
@@ -1,11 +1,3 @@
-# Auto-rename Zellij tabs to current directory basename
-function __zellij_tab_name --on-variable PWD
-    if set -q ZELLIJ
-        command zellij action rename-tab (basename $PWD)
-    end
-end
-
-# Rename tab 1 on session start (--on-variable PWD doesn't fire until first cd)
-if set -q ZELLIJ
-    command zellij action rename-tab (basename $PWD)
-end
+# Zellij tab naming: handled by zellij's built-in process detection.
+# Session name (shown in zjstatus bar) provides project context.
+# Tabs show the running command (nvim, fish, cargo, etc).
